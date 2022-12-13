312f3b77b119ee320f60914229b1306c4a37ba6c
ldanielabe <lauridani1@gmail.com>
2022-12-13 18:06:23 -0500

    storybook

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,7 +1,33 @@
 import './mylabel.css'
 
-export const MyLabel = () => {
+export interface Props{
+  /**
+   * Is this the name of the label
+   */
+  label: string,
+  /**
+   * Is this the size of the label
+   */
+  size: 'normal'|'h1'|'h2'|'h3',
+  /**
+   * When it is true the label will have capital letter
+   */
+  allCaps?: boolean,
+  /**
+   * Is this the color of the label
+   */
+  color?: 'primary'|'secondary'|'tertiary'
+
+}
+export const MyLabel = ({
+  allCaps= false,
+  color='primary',
+  label= 'No Label',
+  size= 'normal'
+}: Props) => {
   return (
-    <div>MyLabel</div>
+    <div className={`${size} text-${color}`}>
+      {allCaps?label.toUpperCase():label}
+    </div>
   )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -1,14 +1,33 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { MyLabel } from "../../components/MyLabel";
 
 export default{
-    title: 'Example/MyLabel',
+    title: 'UI/MyLabel',
     component: MyLabel
-}
+} as ComponentMeta<typeof MyLabel>
 
-const Template = () => <MyLabel/>
+const Template:ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args}/>
 
 export const Basic = Template.bind({})
+Basic.args={
+    size: 'normal',
+    allCaps: false
+}
 
 export const AllCaps = Template.bind({})
+AllCaps.args={
+    size: 'normal',
+    allCaps: true
+}
 
 export const Secondary = Template.bind({})
+Secondary.args={
+    size: 'h2',
+    color: 'secondary'
+}
+
+export const Tertiary = Template.bind({})
+Tertiary.args={
+    size: 'h3',
+    color: 'tertiary'
+}
